1ac79f1f36eeff4c7893e839fe505769667a17f7
JazzyJazz <83658991+JazzyJazz@users.noreply.github.com>
2023-03-27 16:58:22 +0200

    Fin ex semaine 1

--- vecteur3D.cpp
+++ vecteur3D.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 class Vecteur3D{
@@ -27,5 +28,47 @@ class Vecteur3D{
                 }
             }
             return true;
+        }
+
+        vector<double> get_vect(){
+            return vect;
+        }
+
+        // Opérations
+
+        Vecteur3D add(Vecteur3D Vect2) const{
+            return Vecteur3D(vect[0]+Vect2.vect[0], vect[1]+Vect2.vect[1], vect[2]+Vect2.vect[2]);
+        }
+
+        Vecteur3D soust(Vecteur3D Vect2) const{
+            return Vecteur3D(vect[0]-Vect2.vect[0], vect[1]-Vect2.vect[1], vect[2]-Vect2.vect[2]);
+        }
+
+        Vecteur3D opp() const{
+            return Vecteur3D(-vect[0], -vect[1], -vect[2]);
+        }
+
+        Vecteur3D mult(double a) const{
+            return Vecteur3D(a*vect[0], a*vect[1], a*vect[2]);
+        }
+
+        double prod_scal(Vecteur3D Vect2) const{
+            return vect[0]*Vect2.vect[0] + vect[1]*Vect2.vect[1] + vect[2]*Vect2.vect[2];
+        }
+
+        Vecteur3D prod_vect(Vecteur3D Vect2) const{
+            return Vecteur3D(vect[1]*Vect2.vect[2]-vect[2]*Vect2.vect[1], vect[2]*Vect2.vect[0]-vect[0]*Vect2.vect[2], vect[0]*Vect2.vect[1]-vect[1]*Vect2.vect[0]);
+        }
+
+        double norme2() const{
+            return prod_scal(*this);
+        }
+
+        double norme() const{
+            return sqrt(norme2());
+        }
+
+        Vecteur3D unitaire() const{
+            return mult(1.0/norme())
         }
 };
